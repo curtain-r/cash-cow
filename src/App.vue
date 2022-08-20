7c038a43ed3ad9be9e04ebde4d7a1b5e4bea70bf
sam <sam@MacBook-Pro-2.local>
2022-08-20 18:44:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Home from './page/home/index.vue';
+
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/page/home/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import  LayoutHeader  from "./layout-header.vue";
+import LayoutMain from "./layout-main.vue"
+</script>
+<template>
+    <layout-header />
+    <layout-main />
+</template>
